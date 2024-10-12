c0fd5c257c15a0fb221ffa099556a0fd0b8cc78b
Leo-Dev <leonardocaimmi1@gmail.com>
2024-10-12 11:41:28 -0300

    config h2

--- src/main/java/com/miTurno/backend/BackendApplication.java
+++ src/main/java/com/miTurno/backend/BackendApplication.java
@@ -8,6 +8,16 @@ public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+		//http://localhost:8080/swagger-ui/index.html |entrar a swagger
+		//http://localhost:8080/h2-console | entrar en H2 bbdd
+
+
+		//probar en la bbdd
+		/*INSERT INTO USUARIOS (NOMBRE , APELLIDO , CORREO_ELECTRONICO , CELULAR ,FECHA_NACIMIENTO , ROL ) VALUES
+		('Juan', 'Pérez', 'juan.perez@example.com', '123456789', '1990-05-15', 'CLIENTE'),
+		('María', 'Gómez', 'maria.gomez@example.com', '987654321', '1992-08-20', 'ADMIN'),
+		('Pedro', 'Sánchez', 'pedro.sanchez@example.com', '555123456', '1988-12-01', 'CLIENTE');*/
+
 	}
 
 }
