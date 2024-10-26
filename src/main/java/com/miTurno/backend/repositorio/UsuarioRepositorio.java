b84557d598d5111d86bb90d3c0ae34e95431de09
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2024-10-26 12:51:00 -0300

    UPDATE
    Agregue el método para buscar usuario por rol

--- src/main/java/com/miTurno/backend/repositorio/UsuarioRepositorio.java
+++ src/main/java/com/miTurno/backend/repositorio/UsuarioRepositorio.java
@@ -2,8 +2,10 @@ package com.miTurno.backend.repositorio;
 
 import com.miTurno.backend.entidad.UsuarioEntidad;
 import com.miTurno.backend.modelo.Usuario;
+import com.miTurno.backend.tipos.RolUsuarioEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,4 +13,5 @@ import java.util.List;
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<UsuarioEntidad,Long> {
 
+    List<UsuarioEntidad> findByRolUsuarioEnum(RolUsuarioEnum rolUsuarioEnum);
 }
