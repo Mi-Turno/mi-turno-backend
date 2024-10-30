3ff7a2cc3b7c75008d9cfbf2bb9867882ee50bdd
Fabri-dev <fabriziogolisciano@estudiante.mdp.utn.edu.ar>
2024-10-30 10:29:38 -0300

    query servicios por profesionales

--- src/main/java/com/miTurno/backend/repositorio/pivotRepositorios/ServicioXProfesionalesRepositorio.java
+++ src/main/java/com/miTurno/backend/repositorio/pivotRepositorios/ServicioXProfesionalesRepositorio.java
@@ -1,5 +1,18 @@
 package com.miTurno.backend.repositorio.pivotRepositorios;
 
-public interface ServicioXProfesionalesRepositorio {
-    //todo terminarlo xd
+import com.miTurno.backend.entidad.pivotEntidad.ServiciosXProfesionalEntidad;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ServicioXProfesionalesRepositorio extends JpaRepository<ServiciosXProfesionalEntidad,Long> {
+
+    @Query("SELECT s FROM ServicioEntidad AS s " +
+            "JOIN ServiciosXProfesionalEntidad AS sxp ON s.idServicio = sxp.idServicio " +
+            "JOIN UsuarioEntidad AS u ON sxp.idProfesional = u.idUsuario " +
+            "WHERE  u.idUsuario = :id_profesional")
+    List<Long> findAllServiciosByProfesional(@Param("id_profesional")Long id_profesional);
+
 }
