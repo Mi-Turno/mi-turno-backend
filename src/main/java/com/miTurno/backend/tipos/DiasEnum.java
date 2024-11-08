28887954f18bae432ce127404cc89018ad304ed8
Fabri-dev <fabriziogolisciano@estudiante.mdp.utn.edu.ar>
2024-11-08 09:25:10 -0300

    cambio de posicion enumDias

--- src/main/java/com/miTurno/backend/tipos/DiasEnum.java
+++ src/main/java/com/miTurno/backend/tipos/DiasEnum.java
@@ -1,11 +1,11 @@
 package com.miTurno.backend.tipos;
 
 public enum DiasEnum {
+    DOMINGO,
     LUNES,
     MARTES,
     MIERCOLES,
     JUEVES,
     VIERNES,
-    SABADO,
-    DOMINGO
+    SABADO
 }
