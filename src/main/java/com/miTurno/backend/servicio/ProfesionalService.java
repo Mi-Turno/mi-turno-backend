16058806042f6747b4ab4deeadccf39a829ee2de
Leo-Dev <leonardocaimmi1@gmail.com>
2024-11-08 20:11:08 -0300

    metodo para asignar un servicio hacia el profesional

--- src/main/java/com/miTurno/backend/servicio/ProfesionalService.java
+++ src/main/java/com/miTurno/backend/servicio/ProfesionalService.java
@@ -3,6 +3,7 @@ package com.miTurno.backend.servicio;
 import com.miTurno.backend.DTO.Profesional;
 import com.miTurno.backend.entidad.NegocioEntidad;
 import com.miTurno.backend.entidad.ProfesionalEntidad;
+import com.miTurno.backend.entidad.ServicioEntidad;
 import com.miTurno.backend.excepcion.*;
 import com.miTurno.backend.mapper.ProfesionalMapper;
 import com.miTurno.backend.mapper.UsuarioMapper;
@@ -11,6 +12,7 @@ import com.miTurno.backend.request.ProfesionalRequest;
 import com.miTurno.backend.tipos.RolUsuarioEnum;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -24,7 +26,11 @@ public class ProfesionalService {
     private final UsuarioRepositorio usuarioRepositorio;
     private final CredencialesRepositorio credencialesRepositorio;
 
-    public ProfesionalService(ProfesionalRepositorio profesionalRepositorio, RolRepositorio rolRepositorio, UsuarioService usuarioService, UsuarioMapper usuarioMapper, NegocioRepositorio negocioRepositorio, ProfesionalMapper profesionalMapper, UsuarioRepositorio usuarioRepositorio,CredencialesRepositorio credencialesRepositorio) {
+    private final ServicioRepositorio servicioRepositorio;
+
+    public ProfesionalService(ProfesionalRepositorio profesionalRepositorio, RolRepositorio rolRepositorio, UsuarioService usuarioService, UsuarioMapper usuarioMapper,
+                              NegocioRepositorio negocioRepositorio, ProfesionalMapper profesionalMapper, UsuarioRepositorio usuarioRepositorio,CredencialesRepositorio credencialesRepositorio,
+                              ServicioRepositorio servicioRepositorio) {
         this.profesionalRepositorio = profesionalRepositorio;
         this.rolRepositorio = rolRepositorio;
         this.usuarioService = usuarioService;
@@ -33,6 +39,7 @@ public class ProfesionalService {
         this.profesionalMapper = profesionalMapper;
         this.usuarioRepositorio = usuarioRepositorio;
         this.credencialesRepositorio = credencialesRepositorio;
+        this.servicioRepositorio = servicioRepositorio;
     }
 
 
@@ -75,5 +82,32 @@ public class ProfesionalService {
     public Profesional obtenerUnProfesional(Long idProfesional){
         return profesionalMapper.toModel(profesionalRepositorio.findById(idProfesional).orElseThrow(()->new UsuarioNoExistenteException(idProfesional))) ;
     }
+
+    public Profesional asignarUnServicio(Long idProfesional,Long idServicio){
+
+        //Busco los recursos
+        ProfesionalEntidad profesionalEntidad = profesionalRepositorio.findById(idProfesional).orElseThrow(()->new RecursoNoExisteException("Profesional con ID " + idProfesional + " no existe"));
+        ServicioEntidad servicioEntidad = servicioRepositorio.findById(idServicio).orElseThrow(()->new RecursoNoExisteException("Servicio con ID " + idServicio + " no existe"));
+        //Obtengo los servicios del profesional y los profesionales del servicio
+
+        List<ServicioEntidad> listaServicios  = profesionalEntidad.getListaServiciosEntidad();
+        List<ProfesionalEntidad> listaProfesionales  = servicioEntidad.getProfesionales();
+
+        //los matcheo
+        if (!listaServicios.contains(servicioEntidad)) {
+            listaServicios.add(servicioEntidad); // Agrego el servicio al profesional
+        }
+
+        if (!listaProfesionales.contains(profesionalEntidad)) {
+            listaProfesionales.add(profesionalEntidad); // Agrego el profesional al servicio
+        }
+
+        //los guardo
+        servicioRepositorio.save(servicioEntidad);
+        return profesionalMapper.toModel(profesionalRepositorio.save(profesionalEntidad));
+    }
+
+
+
 
 }
