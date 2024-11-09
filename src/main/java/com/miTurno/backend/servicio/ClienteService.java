5c7a68c42d8b7e1b17c74c72d8ebcdb030b15935
Leo-Dev <leonardocaimmi1@gmail.com>
2024-11-09 00:56:00 -0300

    Add cliente

--- src/main/java/com/miTurno/backend/servicio/ClienteService.java
+++ src/main/java/com/miTurno/backend/servicio/ClienteService.java
@@ -1,69 +1,56 @@
 package com.miTurno.backend.servicio;
 
+import com.miTurno.backend.DTO.Cliente;
 import com.miTurno.backend.entidad.ClienteEntidad;
-import com.miTurno.backend.entidad.NegocioEntidad;
-import com.miTurno.backend.entidad.UsuarioEntidad;
-import com.miTurno.backend.excepcion.RecursoNoExisteException;
-import com.miTurno.backend.repositorio.ClienteRepositorio;
-import com.miTurno.backend.repositorio.NegocioRepositorio;
-import com.miTurno.backend.repositorio.UsuarioRepositorio;
+import com.miTurno.backend.excepcion.*;
+import com.miTurno.backend.mapper.ClienteMapper;
+import com.miTurno.backend.repositorio.*;
 import com.miTurno.backend.request.UsuarioRequest;
+import com.miTurno.backend.tipos.RolUsuarioEnum;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClienteService {
 
-//    private final NegocioRepositorio negocioRepositorio;
-//    private final UsuarioRepositorio usuarioRepositorio;
-//    ClienteRepositorio clienteRepositorio;
-//
-//    @Autowired
-//    public ClienteService(ClienteRepositorio clienteRepositorio, NegocioRepositorio negocioRepositorio, UsuarioRepositorio usuarioRepositorio) {
-//        this.clienteRepositorio = clienteRepositorio;
-//        this.negocioRepositorio = negocioRepositorio;
-//        this.usuarioRepositorio = usuarioRepositorio;
-//    }
-//
-//    // Agregar un cliente a un negocio
-//    public ClienteEntidad agregarCliente(Long idNegocio, UsuarioRequest clienteRequest) {
-//
-//
-//        //Si el negocio no existe tiro excepcion
-//        NegocioEntidad negocio = negocioRepositorio.findById(idNegocio)
-//                .orElseThrow(() -> new RecursoNoExisteException("Negocio no encontrado"));
-//
-//
-//
-//        ClienteEntidad nuevoCliente = new ClienteEntidad();
-//        nuevoCliente.setNombre(clienteRequest.getNombre());
-//        nuevoCliente.setApellido(clienteRequest.getApellido());
-//        nuevoCliente.setTelefono(clienteRequest.getTelefono());
-//        nuevoCliente.setFechaNacimiento(clienteRequest.getFechaNacimiento());
-//        nuevoCliente.setCredenciales(clienteRequest.getCredenciales()); // Ajusta según tu implementación
-//
-//        // Aquí se relaciona el cliente con el negocio
-//        nuevoCliente.setNegocio(negocio); // Asegúrate de tener este método en ClienteEntidad
-//
-//        return clienteRepositorio.save(nuevoCliente);
-//    }
-//
-//    // Obtener todos los clientes de un negocio
-//    public List<ClienteEntidad> obtenerClientesPorNegocio(Long idNegocio) {
-//        // Aquí debes implementar un método en tu repositorio que recupere
-//        // los clientes según el ID del negocio
-//        return clienteRepositorio.findAllByNegocioId(idNegocio);
-//    }
-//
-//    // Obtener un cliente de un negocio por ID
-//    public ClienteEntidad obtenerClientePorId(Long idNegocio, Long idCliente) {
-//        // Primero verifica si el negocio existe
-//        NegocioEntidad negocio = negocioRepositorio.findById(idNegocio)
-//                .orElseThrow(() -> new ResourceNotFoundException("Negocio no encontrado"));
-//
-//        // Luego busca el cliente por ID y verifica que pertenezca al negocio
-//        return clienteRepositorio.findByIdAndNegocioId(idCliente, idNegocio)
-//                .orElseThrow(() -> new ResourceNotFoundException("Cliente no encontrado en este negocio"));
-//    }
+    private final ClienteRepositorio clienteRepositorio;
+    private final RolRepositorio rolRepositorio;
+    private final CredencialesRepositorio credencialesRepositorio;
+    private final ClienteMapper clienteMapper;
+
+    @Autowired
+    public ClienteService(ClienteRepositorio clienteRepositorio, RolRepositorio rolRepositorio, CredencialesRepositorio credencialesRepositorio, ClienteMapper clienteMapper) {
+        this.clienteRepositorio = clienteRepositorio;
+        this.rolRepositorio = rolRepositorio;
+        this.credencialesRepositorio = credencialesRepositorio;
+        this.clienteMapper = clienteMapper;
+    }
+
+
+
+    //Crear un cliente
+    public Cliente crearUnCliente(UsuarioRequest usuarioRequest) throws RolIncorrectoException, RecursoNoExisteException {
+
+        RolUsuarioEnum rolUsuarioEnum = rolRepositorio.findById(usuarioRequest.getIdRolUsuario()).get().getRol();
+
+        if (rolUsuarioEnum != RolUsuarioEnum.CLIENTE) {
+            throw new RolIncorrectoException(RolUsuarioEnum.CLIENTE, rolUsuarioEnum);
+        }
+        if (credencialesRepositorio.findByEmail(usuarioRequest.getEmail()).isPresent()) {
+            throw new EmailYaExisteException(usuarioRequest.getEmail());
+        }
+        if (credencialesRepositorio.findByTelefono(usuarioRequest.getTelefono()).isPresent()) {
+            throw new TelefonoYaExisteException(usuarioRequest.getTelefono());
+        }
+        // Crear el cliente
+        ClienteEntidad clienteEntidad = clienteMapper.toEntidad(usuarioRequest);
+        return clienteMapper.toModel(clienteRepositorio.save(clienteEntidad)) ;
+    }
+// Obtener todos los clientes de un negocio
+
+// Obtener un cliente por ID
+    public Cliente buscarUsuario(Long id){
+        return clienteMapper.toModel(clienteRepositorio.findById(id).orElseThrow(()->new UsuarioNoExistenteException(id)));
+    }
 
 }
