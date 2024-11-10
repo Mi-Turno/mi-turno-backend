d8927c797076ea6fa249da8939ae81e9e5a394f2
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2024-11-10 12:12:07 -0300

    Obtener porfesionales por id negocio y estado

--- src/main/java/com/miTurno/backend/repositorio/ProfesionalRepositorio.java
+++ src/main/java/com/miTurno/backend/repositorio/ProfesionalRepositorio.java
@@ -2,6 +2,7 @@ package com.miTurno.backend.repositorio;
 
 import com.miTurno.backend.entidad.HorarioProfesionalEntidad;
 import com.miTurno.backend.entidad.ProfesionalEntidad;
+import com.miTurno.backend.entidad.ServicioEntidad;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface ProfesionalRepositorio extends JpaRepository<ProfesionalEntidad,Long> {
     List<ProfesionalEntidad> findAllByNegocioEntidad_IdUsuario(Long idNegocio);
     ProfesionalEntidad findByIdUsuarioAndNegocioEntidad_IdUsuario( Long idProfesional,Long idNegocio);
+    List<ProfesionalEntidad> findAllByNegocioEntidad_IdUsuarioAndCredenciales_Estado(Long idNegocio, Boolean estado);
+
 }
