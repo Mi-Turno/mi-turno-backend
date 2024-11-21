d2420d5a1c1ad94922f5b3a2b408edca8bddfec3
facundoprocelli <128741375+facundoprocelli@users.noreply.github.com>
2024-11-20 21:37:12 -0300

    Configuración web patch

--- src/main/java/com/miTurno/backend/configuracion/ConfiguracionWeb.java
+++ src/main/java/com/miTurno/backend/configuracion/ConfiguracionWeb.java
@@ -10,7 +10,7 @@ public class ConfiguracionWeb implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")  // Permite solicitudes solo desde este origen
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("Content-Type", "Authorization") //* esto para permitir todos(es mala practica)
                 .allowCredentials(true);  // Permite el uso de credenciales como cookies
     }
