1af764f71996992b2a1c39f491f28ecc44f2c950
Leo-Dev <leonardocaimmi1@gmail.com>
2024-12-31 16:30:21 -0300

    deprecated

--- src/main/java/com/miTurno/backend/configuracion/WebConfig.java
+++ src/main/java/com/miTurno/backend/configuracion/WebConfig.java
@@ -4,15 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+       /* registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200")  // Permite solicitudes solo desde este origen
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowedHeaders("Content-Type", "Authorization") //* esto para permitir todos(es mala practica)
-                .allowCredentials(true);  // Permite el uso de credenciales como cookies
+                .allowCredentials(true);  // Permite el uso de credenciales como cookies*/
     }
 
 }
