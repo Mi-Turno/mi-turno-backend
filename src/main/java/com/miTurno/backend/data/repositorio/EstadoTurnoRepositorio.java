9ac249f31c72e55eafa46a6e433e3bd6fdbe38ec
Leo-Dev <leonardocaimmi1@gmail.com>
2025-02-17 10:22:55 -0300

    cambiar estado del turno de forma programada
    Cada 3 minutos reviso si hay turnos reservados y si hay verifico su fecha y hora en un lapso de 10 minutos y si se encuentra en ese lapso los pongo en curso solamente si es hacia atras ej 12:00 reviso hasta 11:50

--- src/main/java/com/miTurno/backend/data/repositorio/EstadoTurnoRepositorio.java
+++ src/main/java/com/miTurno/backend/data/repositorio/EstadoTurnoRepositorio.java
@@ -1,11 +1,15 @@
 package com.miTurno.backend.data.repositorio;
 
 import com.miTurno.backend.data.domain.EstadoTurnoEntidad;
+import com.miTurno.backend.data.domain.TurnoEntidad;
 import com.miTurno.backend.tipos.EstadoTurnoEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EstadoTurnoRepositorio extends JpaRepository<EstadoTurnoEntidad,Long> {
     EstadoTurnoEntidad findByEstadoTurno(EstadoTurnoEnum estado);
+    //List<TurnoEntidad> findByEstadoTurno(EstadoTurnoEnum estadoTurnoEnum);
 }
--- src/main/java/com/miTurno/backend/data/repositorio/TurnoRepositorio.java
+++ src/main/java/com/miTurno/backend/data/repositorio/TurnoRepositorio.java
@@ -1,6 +1,7 @@
 package com.miTurno.backend.data.repositorio;
 
 import com.miTurno.backend.data.domain.TurnoEntidad;
+import com.miTurno.backend.tipos.EstadoTurnoEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,5 @@ public interface TurnoRepositorio extends JpaRepository<TurnoEntidad,Long> {
 
    List<TurnoEntidad> findAllByNegocioEntidadId(Long idNegocio);
    TurnoEntidad findByNegocioEntidadIdAndId(Long idNegocio, Long idTurno);
+
 }
